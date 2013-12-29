6180987b385b13a1814f238127a35d867de93391
stefanRitter <stefan@stefanritter.com>
2013-12-29 18:28:42 +0100

    move config into app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 var express = require('express'),
     routes = require('./routes'),
     path = require('path'),
-    app = express();
+    app = express(),
+    errorHandler = require('./routes/error').errorHandler,
+    datastoreURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/trybes';
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -15,7 +17,8 @@ app.use(express.favicon(__dirname + '/app/img/favicon.ico'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser());
+app.use(express.cookieParser('your secret here'));
+//app.use(express.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 if ('development' === app.get('env')) {
@@ -25,7 +28,7 @@ if ('development' === app.get('env')) {
 
 if ('production' === app.get('env')) {
   app.use(express.logger('short'));
-  app.use(express.errorHandler());
+  app.use(errorHandler);
 }
 
 // routes
