60062511c63b41d94a70342e34aa4a55d0fe8049
stefanRitter <stefan@stefanritter.com>
2013-12-31 19:49:29 +0100

    add non persistent sessions

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
-// dependencies
 var express = require('express'),
     routes = require('./routes'),
     path = require('path'),
     app = express(),
     errorHandler = require('./middleware/error'),
-    datastoreURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/trybes';
+    datastoreURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/trybes',
+    sessionStore = require('connect-mongo')(express);
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.enable('strict routing');
-
-app.use(express.compress());
-app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-//app.use(express.session());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// routes
-app.use(app.router);
-routes(app);
-
+// logging and error handling
 if ('development' === app.get('env')) {
   app.use(express.logger('dev'));
   app.use(express.errorHandler());
@@ -34,5 +16,28 @@ if ('production' === app.get('env')) {
   app.use(express.logger('short'));
   errorHandler(app);
 }
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.enable('strict routing');
+
+// sessions
+app.use(express.cookieParser(process.env.COOKIE_SECRET || 'your secret here'));
+app.use(express.session({ secret: process.env.SESSION_SECRET || 'your secret here' }));
+app.use(express.csrf());
+
+// middleware
+app.use(express.compress());
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+
+// routes
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+routes(app);
 
 module.exports = app;
