090a9569f3bfa99476b28186602ce9864fd826ac
stefanRitter <stefan@stefanritter.com>
2014-01-04 17:56:24 +0000

    add email spec

--- test/db/emails-spec.js
+++ test/db/emails-spec.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var assert = require('assert'),
-    mongoose = require('mongoose'),
+require('should');
+
+var mongoose = require('mongoose'),
     Email = require('../../db/emails'),
     datastoreURI = 'mongodb://localhost/trybes-test';
 
@@ -12,13 +13,40 @@ describe('Email', function () {
   });
 
   after(function() {
-    mongoose.connection.close();
+    mongoose.connection.collections.emails.drop(function() {
+      mongoose.connection.close();
+    });
   });
 
 
-  describe('test', function() {
-    it('is a test', function () {
-      assert(true);
+  describe('should validate the email address', function() {
+    it('should save a valid email', function (done) {
+      var validEmail = { _id: 'valid@email.com'};
+
+      Email.create(validEmail, function(err, newEmail) {
+        newEmail._id.should.equal('valid@email.com');
+        done();
+      });
+    });
+
+    it('should reject an invalid email', function (done) {
+      var invalidEmail = { _id: 'invalid'};
+
+      Email.create(invalidEmail, function(err) {
+        err.name.should.equal('ValidationError');
+        done();
+      });
+    });
+
+    it('should not allow dublicates', function (done) {
+      var dub = { _id: 'test@test.com' };
+
+      Email.create(dub, function () {
+        Email.create(dub, function (err) {
+          err.code.should.equal(11000);
+          done();
+        });
+      });
     });
   });
 });
